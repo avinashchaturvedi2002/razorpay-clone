8a888f957f8230bc7f8bff6918876a5af41db16c
Avinash Chaturvedi <avinashchaturvedi2002@gmail.com>
2024-12-31 19:13:58 +0530

    Clone Completed

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mullish","sans-serif"],
+      },
+      colors:{
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+        darkBlue:"#032a3e",
+        marqcolor:"#009e5c",
+        warncolor:"rgba(233, 105, 12, 0.18)"
+      }
+    },
+  },
+  plugins: []
+}
+
